acfe65120f984497acdc36153b2ca24160f1ae42
linsj <lin.sujie1234@gmail.com>
2019-10-18 11:44:35 +0800

    add modulation support

--- src/DataPlots.jl
+++ src/DataPlots.jl
@@ -5,8 +5,32 @@ module DataPlots
 export plot_BC
 export plot_proton
 export plot_pbar
+export modulation
 
 using Plots
+using Interpolations
+
+"""
+    modulation(ene::Array{T,1} where {T<:Real}, flux::Array{T,1} where {T<:Real};
+               A::Int = 1, Z::Int = 1, phi::Real = 0)
+
+Doing solar modulation for specified spectrum; ene in unit [GeV]; flux is the nucleon flux
+
+# Arguments
+* `A`,`Z`:  the A and Z of the particle
+* `phi`:    modulation potential [unit: GV]
+"""
+function modulation(ene::Array{T,1} where {T<:Real}, flux::Array{T,1} where {T<:Real}; A::Int = 1, Z::Int = 1, phi::Real = 0)
+  phi_ = phi * abs(Z) / A
+  m0 = A == 0 ? 0.511e-3 : 0.9382
+
+  logene = log.(ene)
+  dloge = logene[2] - logene[1]
+  itpspec = interpolate((logene[1]:dloge:last(logene),), log.(flux), Gridded(Linear()))
+  spec = extrapolate(itpspec, Line())
+
+  (ene, map(e-> e * (e + 2 * m0) / ( (e + phi_) * (e + phi_ + 2 * m0)) * exp(spec(log(e + phi_))), ene))
+end
 
 function get_data(fname::String; index::Real = 0.0, norm::Real = 1.0)
   basedir = dirname(@__FILE__)
@@ -75,8 +99,10 @@ function plot_pbar(spectra::Array{Dict{String,Array{Float64,1}},1}, label::Array
   plot_data(data["AMS2016nonformal(0000/00)"])
   plot!(xaxis=:log, yaxis=:log)
 
-  proton = map(spec-> (spec["secondary_antiprotons"] + spec["tertiary_antiprotons"]) .* (spec["eaxis"] .^ 2), spectra)
-  plot!(spectra[1]["eaxis"], proton; label = label)
+  #pbar = map(spec-> (spec["secondary_antiprotons"] + spec["tertiary_antiprotons"]) .* (spec["eaxis"] .^ 2), spectra)
+  pbar = map(spec-> max.(spec["DM_antiprotons"] .* (spec["eaxis"] .^ 2), 1e-7), spectra)
+  print(pbar)
+  plot!(spectra[1]["eaxis"], pbar; label = label)
 end
 
 end # module
