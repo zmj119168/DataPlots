b5e4d4dd9a5a668623ef537085858196172ef7fc
zmj119168 <57001155+zmj119168@users.noreply.github.com>
2020-08-20 15:29:37 +0800

    Update DataPlots.jl

--- src/DataPlots.jl
+++ src/DataPlots.jl
@@ -281,14 +281,14 @@ end
 """
 function plot_pbar(spectra::Array{Dict{String,Particle},1}, label::Array{String,2} = Array{String,2}(undef, (0,0)); phi::Real = 0, data::Array{String,1}=["AMS2016nonformal(2011/05/19-2015/05/26)"],k::Real = 1)
   plot_comparison(spec -> rescale(spec["secondary_antiprotons"] + spec["tertiary_antiprotons"], 2.7) * 1e4*k,
-                  spectra, label; phi=phi, data=data, datafile="pbar.dat",index=-2.7, yscale=:log, ylabel="\$E^{2.7}dN/dE [GeV^{2.7}(m^{2}*sr*s*GeV)^{-1}]\$")
+                  spectra, label; phi=phi, data=data, datafile="pbar.dat",index=-2.7, ylabel="\$E^{2.7}dN/dE [GeV^{2.7}(m^{2}*sr*s*GeV)^{-1}]\$")
  #plot_comparison(spec -> rescale(spec["DM_antiprotons"], 2.0) * 1e-3,
  #                spectra, label; phi=phi, data=data, datafile="pbar.dat",index=-2, yscale=:log, ylabel="\$E^{2}dN/dE [GeV^{2}(m^{2}*sr*s*GeV)^{-1}]\$")
 end
 
 function plot_pbarp(spectra::Array{Dict{String,Particle},1}, label::Array{String,2} = Array{String,2}(undef, (0,0)); phi::Real = 0, data::Array{String,1}=["AMS02rigidity(2011/05-2015/05)"])
   plot_comparison(spec -> (spec["secondary_antiprotons"] + spec["tertiary_antiprotons"])/(spec["Hydrogen_1"] +spec["secondary_protons"]) ,
-                  spectra, label; phi=phi, data=data, datafile="pbarp.dat", yscale=:log, ylabel="\$ \bar{p}/p \$")
+                  spectra, label; phi=phi, data=data, datafile="pbarp.dat", yscale=:log, ylabel=" bar{p}/p ")
  #plot_comparison(spec -> rescale(spec["DM_antiprotons"], 2.0) * 1e-3,
  #                spectra, label; phi=phi, data=data, datafile="pbar.dat",index=-2, yscale=:log, ylabel="\$E^{2}dN/dE [GeV^{2}(m^{2}*sr*s*GeV)^{-1}]\$")
 end
